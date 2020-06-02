56c5437bc75c4a4f1a39e620ee2777c42c51809e
Luke Batchelor (Automated) <lbatchelor@atlassian.com>
2020-06-02 13:46:31 +0000

    Automatic commit created Tue Jun  2 13:46:30 UTC 2020

---
+++ design-system/dynamic-table/examples/18-stateless-with-sorting-and-pagination.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+import styled from 'styled-components';
+
+import Button, { ButtonGroup } from '@atlaskit/button';
+
+import { DynamicTableStateless } from '../src';
+import { SortOrderType } from '../src/types';
+
+import { caption, head, rows } from './content/sample-data';
+
+const Wrapper = styled.div`
+  min-width: 600px;
+`;
+
+const DefaultComponent = () => {
+  const [pageNumber, setPageNumber] = useState<number>(2);
+  const [sortOrder, setSortOrder] = useState<SortOrderType>('ASC');
+  const [sortKey, setSortKey] = useState<any>(2);
+
+  const onSort = ({ key, sortOrder }: any) => {
+    setSortKey(key);
+    setSortOrder(sortOrder);
+  };
+
+  const navigateTo = (pageNumber: number) => setPageNumber(pageNumber);
+
+  return (
+    <Wrapper>
+      <ButtonGroup>
+        <Button
+          isDisabled={pageNumber === 1}
+          onClick={() => navigateTo(pageNumber - 1)}
+        >
+          Previous Page
+        </Button>
+        <Button
+          isDisabled={pageNumber === 5}
+          onClick={() => navigateTo(pageNumber + 1)}
+        >
+          Next Page
+        </Button>
+      </ButtonGroup>
+      <DynamicTableStateless
+        caption={caption}
+        head={head}
+        rows={rows}
+        rowsPerPage={10}
+        page={pageNumber}
+        loadingSpinnerSize="large"
+        isLoading={false}
+        isFixedSize
+        sortKey={sortKey}
+        sortOrder={sortOrder}
+        onSort={onSort}
+        onSetPage={navigateTo}
+      />
+    </Wrapper>
+  );
+};
+
+export default DefaultComponent;

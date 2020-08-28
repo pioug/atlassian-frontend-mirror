2e34251ea20d05797b3aad275f5760be470c458d
Luke Batchelor (Automated) <lbatchelor@atlassian.com>
2020-08-28 13:46:05 +0000

    Automatic commit created Fri Aug 28 13:46:05 UTC 2020

--- editor/editor-core/src/__tests__/integration/date/quick-insert.ts
+++ editor/editor-core/src/__tests__/integration/date/quick-insert.ts
@@ -1,4 +1,4 @@
-import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
+import { BrowserTestCase, Browser } from '@atlaskit/webdriver-runner/runner';
 import { editable, quickInsert, getDocFromElement } from '../_helpers';
 import {
   goToEditorTestingExample,
@@ -8,28 +8,9 @@ import {
 
 const dateLozenge = 'span[timestamp]';
 
-// ED-10171 Global date mock is currently flaky
-// BrowserTestCase(
-//   'quick-insert.ts: Insert date via quick insert with global date mock intact',
-//   { skip: ['firefox', 'edge'] },
-//   async (client: any, testName: string) => {
-//     const page = await goToEditorTestingExample(client);
-
-//     await mountEditor(page, {
-//       appearance: 'full-page',
-//       allowDate: true,
-//     });
-
-//     await page.click(editable);
-//     await quickInsert(page, 'Date');
-
-//     const doc = await page.$eval(editable, getDocFromElement);
-//     expect(doc).toMatchCustomDocSnapshot(testName);
-//   },
-// );
 BrowserTestCase(
   'quick-insert.ts: Insert date via quick insert',
-  { skip: ['firefox', 'edge'] },
+  { skip: ['edge'] },
   async (client: any, testName: string) => {
     const page = await goToEditorTestingExample(client);
 
@@ -41,7 +22,7 @@ BrowserTestCase(
       minute: 0,
       tz: 11,
     };
-    page.mockDate(JAN_1ST_2019_AEST_TIMEZONE);
+    await page.mockDate(JAN_1ST_2019_AEST_TIMEZONE);
     // 1st Jan 2019 00:00 AEST / 31st Dec 2018 13:00 UTC
 
     await mountEditor(page, {
@@ -55,45 +36,13 @@ BrowserTestCase(
 
     const doc = await page.$eval(editable, getDocFromElement);
     expect(doc).toMatchCustomDocSnapshot(testName);
+    await page.teardownMockDate();
   },
 );
 
-// ED-10171 Global date mock is currently flaky
-// BrowserTestCase(
-//   'quick-insert.ts: Insert date with global date mock working correctly after a reset',
-//   { skip: ['firefox', 'edge'] },
-//   async (client: any, testName: string) => {
-//     const page = await goToEditorTestingExample(client);
-
-//     const JAN_1ST_2019_AEST_TIMEZONE = {
-//       year: 2019,
-//       monthIndex: 0,
-//       day: 1,
-//       hour: 0,
-//       minute: 0,
-//       tz: 11,
-//     };
-
-//     page.mockDate(JAN_1ST_2019_AEST_TIMEZONE);
-
-//     page.mockStandardDate();
-
-//     await mountEditor(page, {
-//       appearance: 'full-page',
-//       allowDate: true,
-//     });
-//     await quickInsert(page, 'Date');
-//     expect(await page.getText(dateLozenge)).toBe('Aug 16, 2017');
-
-//     const newDoc = await page.$eval(editable, getDocFromElement);
-//     expect(newDoc).toMatchCustomDocSnapshot(testName);
-
-//   },
-// );
-
 BrowserTestCase(
   "quick-insert.ts: Uses today's date in user's local timezone as initial selection",
-  { skip: ['firefox', 'edge', 'safari'] },
+  { skip: ['edge'] },
   async (client: any) => {
     const page = await goToEditorTestingExample(client);
 
@@ -105,7 +54,7 @@ BrowserTestCase(
       minute: 0,
       tz: 11,
     };
-    page.mockDate(JAN_1ST_2019_AEST_TIMEZONE);
+    await page.mockDate(JAN_1ST_2019_AEST_TIMEZONE);
     // 1st Jan 2019 00:00 AEST / 31st Dec 2018 13:00 UTC
 
     await mountEditor(page, {
@@ -117,12 +66,13 @@ BrowserTestCase(
     await quickInsert(page, 'Date');
 
     expect(await page.getText(dateLozenge)).toBe('Jan 1, 2019');
+    await page.teardownMockDate();
   },
 );
 
 BrowserTestCase(
   'quick-insert.ts: format date to localized version',
-  { skip: ['firefox', 'edge', 'safari'] },
+  { skip: ['edge'] },
   async (client: any) => {
     const page = await goToEditorTestingExample(client);
     await loadLocale(page, ['pt', 'es']);
@@ -135,7 +85,7 @@ BrowserTestCase(
       minute: 0,
       tz: 11,
     };
-    page.mockDate(JAN_1ST_2019_AEST_TIMEZONE);
+    await page.mockDate(JAN_1ST_2019_AEST_TIMEZONE);
     // 1st Jan 2019 00:00 AEST / 31st Dec 2018 13:00 UTC
 
     await mountEditor(
@@ -150,7 +100,18 @@ BrowserTestCase(
     await page.click(editable);
     await quickInsert(page, 'Date');
 
-    expect(await page.getText(dateLozenge)).toBe('1 de jan. de 2019');
+    const lozengeText = await page.getText(dateLozenge);
+    // Get the browser name from the client object
+    // This object is current untyped, and Safari is capitalised while firefox and chrome aren't.
+    const browser = (client.capabilities
+      .browserName as string).toLowerCase() as Browser;
+
+    // Safari formats dates slightly differently
+    if (browser === 'safari') {
+      expect(lozengeText).toBe('1 de jan de 2019');
+    } else {
+      expect(lozengeText).toBe('1 de jan. de 2019');
+    }
 
     await mountEditor(
       page,
@@ -163,6 +124,13 @@ BrowserTestCase(
 
     await page.click(editable);
     await quickInsert(page, 'Date');
-    expect(await page.getText(dateLozenge)).toBe('1 ene. 2019');
+
+    const lozengeText2 = await page.getText(dateLozenge);
+    if (browser === 'safari') {
+      expect(lozengeText2).toBe('1 ene 2019');
+    } else {
+      expect(lozengeText2).toBe('1 ene. 2019');
+    }
+    await page.teardownMockDate();
   },
 );

a53372abdf47b138cec9d5c9cdd8afcfbe2279d1
Luke Batchelor (Automated) <lbatchelor@atlassian.com>
2021-02-05 13:33:12 +0000

    Automatic commit created Fri Feb  5 13:33:12 UTC 2021

---
+++ editor/editor-core/src/__tests__/integration/development-bundle/forbidden-packages.ts
@@ -0,0 +1,8 @@
+test.skip.each(['enzyme', 'jest'])('%s should not be bundled', async name => {
+  await expect(name).not.toBeBundled({
+    chunks: ['packages/editor/editor-core/examples'],
+  });
+});
+
+// avoid --isolatedModules error
+export {};

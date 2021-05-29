9f442740facbc093d81376a1bbeb8381822a7349
Luke Batchelor (Automated) <lbatchelor@atlassian.com>
2021-05-29 13:33:24 +0000

    Automatic commit created Sat May 29 13:33:24 UTC 2021

--- dragonfruit/components/examples/00-basic.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import ComponentList from '../src';
-
-export default function Basic() {
-  return <ComponentList testId="component-list" />;
-}
--- dragonfruit/components/src/ui/component-list/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { Wrapper } from './styled';
-import { ComponentListProps } from './types';
-
-export default function ComponentList({ testId }: ComponentListProps) {
-  return <Wrapper testId={testId}>Hello world 👋</Wrapper>;
-}

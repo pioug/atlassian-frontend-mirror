199aa96196acaee355d1e1914048db5bd886ae01
AFP Repo Bot (Automated) <afp-repo-bot@atlassian.com>
2022-09-04 13:33:26 +0000

    Automatic commit created Sun Sep  4 13:33:26 UTC 2022

--- elements/status/src/__tests__/visual-regression/statusSnapshotTest.ts
+++ elements/status/src/__tests__/visual-regression/statusSnapshotTest.ts
@@ -23,7 +23,8 @@ describe('Snapshot Test', () => {
     expect(image).toMatchProdImageSnapshot();
   });
 
-  it('status picker', async () => {
+  // FIXME: This test was automatically skipped due to failure on 03/09/2022: https://product-fabric.atlassian.net/browse/ED-15571
+  it.skip('status picker', async () => {
     const url = getExampleUrl(
       'elements',
       'status',

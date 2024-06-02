5e0284eb7be6bcd620e75c989ec6ac3cbbbe9afc
AFP Repo Bot (Automated) <bdlg5n34dzsu8jiyssvbqlprs5sq25@bots.bitbucket.org>
2024-06-02 13:33:27 +0000

    Automatic commit created Sun Jun  2 13:33:27 UTC 2024

--- confluence/embedded-confluence-public/scripts/devCleanup.js
+++ confluence/embedded-confluence-public/scripts/devCleanup.js
@@ -6,14 +6,8 @@ const CACHED_PKG_PATH = path.resolve(__dirname, '../cached-package.json');
 
 const pkgData = JSON.parse(fs.readFileSync(ORIG_PKG_PATH));
 
-const devDepsToRemove = ['@atlassian/atlassian-frontend-prettier-config-1.0.0'];
-
 fs.writeFile(CACHED_PKG_PATH, JSON.stringify(pkgData), function (err) {
   if (err) throw err;
-});
-
-devDepsToRemove.forEach(function (pkgName) {
-  delete pkgData.devDependencies[pkgName];
 });
 
 delete pkgData.optionalDependencies;

81e0bfa0e69dbb5f0be70b5ef888f55505afe973
AFP Repo Bot (Automated) <bdlg5n34dzsu8jiyssvbqlprs5sq25@bots.bitbucket.org>
2026-08-11 13:35:47 +0000

    Automatic commit created Tue Aug 11 13:35:47 UTC 2026

--- editor/editor-common/src/block-menu/getLocalIdSelector.ts
+++ editor/editor-common/src/block-menu/getLocalIdSelector.ts
@@ -1,40 +1,38 @@
-export const getLocalIdSelector = (
-  localId: string,
-  container: HTMLElement,
-): HTMLElement | null => {
-  // Check if the element with data-local-id exists
-  let element = container.querySelector(
-    `[data-local-id="${localId}"]`,
-  ) as HTMLElement | null;
+import { fg } from '@atlaskit/platform-feature-flags';
 
-  if (element) {
-    return element;
-  }
+export const getLocalIdSelector = (localId: string, container: HTMLElement): HTMLElement | null => {
+	// Check if the element with data-local-id exists
+	let element = container.querySelector(`[data-local-id="${localId}"]`) as HTMLElement | null;
 
-  // Special case for decision lists and task lists which already have localId
-  element = container.querySelector(
-    `[data-decision-list-local-id="${localId}"]`,
-  );
-  if (element) {
-    return element;
-  }
+	if (element) {
+		return element;
+	}
 
-  element = container.querySelector(`[data-task-list-local-id="${localId}"]`);
-  if (element) {
-    return element;
-  }
+	// Special case for decision lists and task lists which already have localId
+	element = container.querySelector(`[data-decision-list-local-id="${localId}"]`);
+	if (element) {
+		return element;
+	}
 
-  // Special case for tables which use data-table-local-id
-  element = container.querySelector(`[data-table-local-id="${localId}"]`);
-  if (element) {
-    return element.parentElement; // return table wrapper instead of table div, so the height calculation is correct
-  }
+	element = container.querySelector(`[data-task-list-local-id="${localId}"]`);
+	if (element) {
+		return element;
+	}
 
-  // Special case for extension, smart cards and media which use lowercase localid
-  element = container.querySelector(`[localid="${localId}"]`);
-  if (element) {
-    return element;
-  }
+	// Special case for tables which use data-table-local-id
+	element = container.querySelector(`[data-table-local-id="${localId}"]`);
+	if (element) {
+		if (fg('platform_editor_block_menu_v2_patch_4')) {
+			return element.parentElement; // return table wrapper instead of table div, so the height calculation is correct
+		}
+		return element;
+	}
 
-  return null;
+	// Special case for extension, smart cards and media which use lowercase localid
+	element = container.querySelector(`[localid="${localId}"]`);
+	if (element) {
+		return element;
+	}
+
+	return null;
 };
